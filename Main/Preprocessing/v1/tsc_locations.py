a63b4301e81e9e7b39cf3b72ed7c1357807f2330
Joel Thomas <55971847+joelt1@users.noreply.github.com>
2021-07-25 16:36:50 +1000

    Initial upload

---
+++ Main/Preprocessing/v1/tsc_locations.py
@@ -0,0 +1,533 @@
+ # 1) and 2)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "MILTON",
+  # The second hierarchical level of controller grouping. Typically there is only one intersection per subsystem, but more may be allocated if the intersections are in close proximity
+  "subsystem" : 2446,
+  # The suburb the controller is in
+  "tsc" : 87,
+  # Name of intersection based on the TSC, which is displayed on the physical controller
+  "areaNum" : "B0087",
+  "coordinates" : {
+    # Australian Map Grid coordinates
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 500520,
+      "northing" : 6961165
+    },
+    # Gregory's street directories coordinates
+    "ubd" : {
+      "page" : 21,
+      "grid" : "M5"
+    },
+    "latLng" : {
+      "latitude" : -27.47308,
+      "longitude" : 153.005263
+    }
+  },
+  # All roads leading into and leaving the intersection
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 285,
+    "streetName" : "GRAHAM STREET",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 1,
+    "rightLaneCount" : 1,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 45,
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 3,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 320,
+    "streetName" : "PARK ROAD",
+    "slipLaneCount" : 2,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 1,
+    "nextTsc" : 293,
+    "detectors" : [ ]
+  }, {
+    "arm" : 4,
+    # The angle of street leading into the intersection
+    "azimuth" : 55,
+    "streetName" : "CORONATION DRIVE",
+    # Number of slip lanes
+    "slipLaneCount" : 0,
+    # The number of lanes leading into the intersection
+    "leftLaneCount" : 3,
+    # The number of lanes leaving the intersection
+    "rightLaneCount" : 3,
+    # The intersection which is adjacent to this one along this arm
+    "nextTsc" : 188,
+    "detectors" : [ ]
+  }, {
+    "arm" : 5,
+    "azimuth" : 225,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 1,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 2,
+    "nextTsc" : 203,
+    "detectors" : [ ]
+  } ]
+},
+ # 3) and 4)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "TOOWONG",
+  "subsystem" : 2447,
+  "tsc" : 88,
+  "areaNum" : "B0088",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 499670,
+      "northing" : 6960127
+    },
+    "ubd" : {
+      "page" : 21,
+      "grid" : "E13"
+    },
+    "latLng" : {
+      "latitude" : -27.482451,
+      "longitude" : 152.99666
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 292,
+    "streetName" : "SYLVAN ROAD",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 1,
+    "rightLaneCount" : 1,
+    "nextTsc" : 362,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 25,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 3,
+    "nextTsc" : 364,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 210,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 2,
+    "nextTsc" : 278,
+    "detectors" : [ ]
+  } ]
+},
+ # 5) and 6)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "CITY",
+  "subsystem" : 2458,
+  "tsc" : 132,
+  "areaNum" : "B0132",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 501506,
+      "northing" : 6961798
+    },
+    "ubd" : {
+      "page" : 18,
+      "grid" : "D19"
+    },
+    "latLng" : {
+      "latitude" : -27.467365,
+      "longitude" : 153.015243
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 260,
+    "streetName" : "CORONATION DR",
+    "slipLaneCount" : 1,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 0,
+    "nextTsc" : 221,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 0,
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 2,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 340,
+    "streetName" : "SKEW ST",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 0,
+    "rightLaneCount" : 2,
+    "nextTsc" : 213,
+    "detectors" : [ ]
+  }, {
+    "arm" : 4,
+    "azimuth" : 0,
+    "streetName" : "SAUL ST",
+    "slipLaneCount" : 2,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 0,
+    "nextTsc" : 40,
+    "detectors" : [ ]
+  }, {
+    "arm" : 5,
+    "azimuth" : 80,
+    "streetName" : "CORONATION DR",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 0,
+    "rightLaneCount" : 1,
+    "nextTsc" : 85,
+    "detectors" : [ ]
+  }, {
+    "arm" : 6,
+    "azimuth" : 170,
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 2,
+    "detectors" : [ ]
+  }, {
+    "arm" : 7,
+    "azimuth" : 180,
+    "streetName" : "WILLIAM JOLLY BRIDGE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 2,
+    "nextTsc" : 481,
+    "detectors" : [ ]
+  } ]
+},
+ # 7) and 8)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "AUCHENFLOWER",
+  "subsystem" : 2469,
+  "tsc" : 181,
+  "areaNum" : "B0181",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 499955,
+      "northing" : 6960557
+    },
+    "ubd" : {
+      "page" : 21,
+      "grid" : "G10"
+    },
+    "latLng" : {
+      "latitude" : -27.478569,
+      "longitude" : 152.999544
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 315,
+    "streetName" : "CHASELY STREET",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 1,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 40,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 3,
+    "nextTsc" : 203,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 220,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 2,
+    "nextTsc" : 364,
+    "detectors" : [ ]
+  } ]
+},
+ # 9) and 10)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "MILTON",
+  "subsystem" : 2473,
+  "tsc" : 188,
+  "areaNum" : "B0188",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 500831,
+      "northing" : 6961441
+    },
+    "ubd" : {
+      "page" : 21,
+      "grid" : "P2"
+    },
+    "latLng" : {
+      "latitude" : -27.470588,
+      "longitude" : 153.008411
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 340,
+    "streetName" : "CRIBB STREET",
+    "slipLaneCount" : 1,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 1,
+    "nextTsc" : 139,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 45,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 3,
+    "nextTsc" : 221,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 225,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 3,
+    "nextTsc" : 87,
+    "detectors" : [ ]
+  } ]
+},
+ # 11) and 12)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "AUCHENFLOWER",
+  "subsystem" : 2475,
+  "tsc" : 203,
+  "areaNum" : "B0203",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 500296,
+      "northing" : 6960915
+    },
+    "ubd" : {
+      "page" : 21,
+      "grid" : "K7"
+    },
+    "latLng" : {
+      "latitude" : -27.475337,
+      "longitude" : 153.002996
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 315,
+    "streetName" : "LANG PARADE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 1,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 45,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 3,
+    "nextTsc" : 87,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 225,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 2,
+    "nextTsc" : 181,
+    "detectors" : [ ]
+  } ]
+},
+ # 13) and 14)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "MILTON",
+  "subsystem" : 2480,
+  "tsc" : 221,
+  "areaNum" : "B0221",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 501081,
+      "northing" : 6961639
+    },
+    "ubd" : {
+      "page" : 159,
+      "grid" : "H11"
+    },
+    "latLng" : {
+      "latitude" : -27.468801,
+      "longitude" : 153.010941
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 325,
+    "streetName" : "HALE STREET",
+    "slipLaneCount" : 2,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 1,
+    "nextTsc" : 291,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 55,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 0,
+    "rightLaneCount" : 3,
+    "nextTsc" : 132,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 145,
+    "streetName" : "GO BETWEEN BRIDGE",
+    "slipLaneCount" : 1,
+    "leftLaneCount" : 1,
+    "rightLaneCount" : 2,
+    "nextTsc" : 846,
+    "detectors" : [ ]
+  }, {
+    "arm" : 4,
+    "azimuth" : 235,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 1,
+    "leftLaneCount" : 5,
+    "rightLaneCount" : 1,
+    "nextTsc" : 188,
+    "detectors" : [ ]
+  } ]
+},
+ # 15) and 16)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "TOOWONG",
+  "subsystem" : 2447,
+  "tsc" : 278,
+  "areaNum" : "B0278",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 499493,
+      "northing" : 6959957
+    },
+    "ubd" : {
+      "page" : 21,
+      "grid" : "D14"
+    },
+    "latLng" : {
+      "latitude" : -27.483986,
+      "longitude" : 152.994868
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 50,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 3,
+    "nextTsc" : 88,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 230,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 3,
+    "nextTsc" : 152,
+    "detectors" : [ ]
+  } ]
+},
+ # 17) and 18)
+ {
+  "region" : "NORMAN",
+  "scatsVersion" : "6.9.4",
+  "suburb" : "AUCHENFLOWER",
+  "subsystem" : 2519,
+  "tsc" : 364,
+  "areaNum" : "B0364",
+  "coordinates" : {
+    "amg" : {
+      "zone" : 56,
+      "hemisphere" : "South",
+      "easting" : 499846,
+      "northing" : 6960423
+    },
+    "ubd" : {
+      "page" : 21,
+      "grid" : "G10"
+    },
+    "latLng" : {
+      "latitude" : -27.479779,
+      "longitude" : 152.998441
+    }
+  },
+  "arms" : [ {
+    "arm" : 1,
+    "azimuth" : 280,
+    "streetName" : "LAND STREET",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 2,
+    "rightLaneCount" : 2,
+    "nextTsc" : 363,
+    "detectors" : [ ]
+  }, {
+    "arm" : 2,
+    "azimuth" : 40,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 0,
+    "leftLaneCount" : 4,
+    "rightLaneCount" : 3,
+    "nextTsc" : 181,
+    "detectors" : [ ]
+  }, {
+    "arm" : 3,
+    "azimuth" : 215,
+    "streetName" : "CORONATION DRIVE",
+    "slipLaneCount" : 1,
+    "leftLaneCount" : 3,
+    "rightLaneCount" : 2,
+    "nextTsc" : 88,
+    "detectors" : [ ]
+  } ]
+},
